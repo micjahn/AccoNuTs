964584d660a3ac3cd9f3cc4ae8f20c83c68523eb
micjahn <mikej06@hotmail.com>
2011-08-24 20:38:37 +0000

    first step to IBAN support
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05]$/acconuts/trunk;C69430

---
+++ AccountNumberTools.Contracts/IBAN/Country.cs
@@ -0,0 +1,245 @@
+﻿//
+//   Project:           AccountNumberTools - Tools for the work with account numbers
+//   Project:           $URL$
+//   Id:                $Id$
+//
+//   Copyright © 2011 Michael Jahn
+//
+//   This Software is weak copyleft open source. Please read the License.txt for details.
+//
+
+using System;
+
+namespace AccountNumberTools.IBAN.Contracts
+{
+   /// <summary>
+   /// enumeration of supported countries
+   /// </summary>
+   public enum Country
+   {
+      /// <summary>
+      /// 
+      /// </summary>
+      Albania,
+      /// <summary>
+      /// 
+      /// </summary>
+      Andorra,
+      /// <summary>
+      /// 
+      /// </summary>
+      Austria,
+      /// <summary>
+      /// 
+      /// </summary>
+      Belgium,
+      /// <summary>
+      /// 
+      /// </summary>
+      Bahrain,
+      /// <summary>
+      /// 
+      /// </summary>
+      BosniaAndHerzegovina,
+      /// <summary>
+      /// 
+      /// </summary>
+      Bulgaria,
+      /// <summary>
+      /// 
+      /// </summary>
+      Croatia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Cyprus,
+      /// <summary>
+      /// 
+      /// </summary>
+      CzechRepublic,
+      /// <summary>
+      /// 
+      /// </summary>
+      Denmark,
+      /// <summary>
+      /// 
+      /// </summary>
+      DominicanRepublic,
+      /// <summary>
+      /// 
+      /// </summary>
+      Estonia,
+      /// <summary>
+      /// 
+      /// </summary>
+      FaroeIslands,
+      /// <summary>
+      /// 
+      /// </summary>
+      Finland,
+      /// <summary>
+      /// 
+      /// </summary>
+      France,
+      /// <summary>
+      /// 
+      /// </summary>
+      Georgia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Germany,
+      /// <summary>
+      /// 
+      /// </summary>
+      Gibraltar,
+      /// <summary>
+      /// 
+      /// </summary>
+      Greece,
+      /// <summary>
+      /// 
+      /// </summary>
+      Greenland,
+      /// <summary>
+      /// 
+      /// </summary>
+      Hungary,
+      /// <summary>
+      /// 
+      /// </summary>
+      Iceland,
+      /// <summary>
+      /// 
+      /// </summary>
+      Ireland,
+      /// <summary>
+      /// 
+      /// </summary>
+      Israel,
+      /// <summary>
+      /// 
+      /// </summary>
+      Italy,
+      /// <summary>
+      /// 
+      /// </summary>
+      Kazakhstan,
+      /// <summary>
+      /// 
+      /// </summary>
+      Kuwait,
+      /// <summary>
+      /// 
+      /// </summary>
+      Latvia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Lebanon,
+      /// <summary>
+      /// 
+      /// </summary>
+      Liechtenstein,
+      /// <summary>
+      /// 
+      /// </summary>
+      Lithuania,
+      /// <summary>
+      /// 
+      /// </summary>
+      Luxembourg,
+      /// <summary>
+      /// 
+      /// </summary>
+      Macedonia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Malta,
+      /// <summary>
+      /// 
+      /// </summary>
+      Mauritania,
+      /// <summary>
+      /// 
+      /// </summary>
+      Mauritius,
+      /// <summary>
+      /// 
+      /// </summary>
+      Monaco,
+      /// <summary>
+      /// 
+      /// </summary>
+      Montenegro,
+      /// <summary>
+      /// 
+      /// </summary>
+      Netherlands,
+      /// <summary>
+      /// 
+      /// </summary>
+      Norway,
+      /// <summary>
+      /// 
+      /// </summary>
+      Poland,
+      /// <summary>
+      /// 
+      /// </summary>
+      Portugal,
+      /// <summary>
+      /// 
+      /// </summary>
+      Romania,
+      /// <summary>
+      /// 
+      /// </summary>
+      SanMarino,
+      /// <summary>
+      /// 
+      /// </summary>
+      SaudiArabia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Serbia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Slovakia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Slovenia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Spain,
+      /// <summary>
+      /// 
+      /// </summary>
+      Sweden,
+      /// <summary>
+      /// 
+      /// </summary>
+      Switzerland,
+      /// <summary>
+      /// 
+      /// </summary>
+      Tunisia,
+      /// <summary>
+      /// 
+      /// </summary>
+      Turkey,
+      /// <summary>
+      /// 
+      /// </summary>
+      UnitedArabEmirates,
+      /// <summary>
+      /// 
+      /// </summary>
+      UnitedKingdom
+   }
+}
---
+++ AccountNumberTools.Contracts/IBAN/IIBANConvert.cs
@@ -0,0 +1,37 @@
+﻿//
+//   Project:           AccountNumberTools - Tools for the work with account numbers
+//   Project:           $URL$
+//   Id:                $Id$
+//
+//   Copyright © 2011 Michael Jahn
+//
+//   This Software is weak copyleft open source. Please read the License.txt for details.
+//
+
+using System;
+
+namespace AccountNumberTools.IBAN.Contracts
+{
+   /// <summary>
+   /// interface for converting from and to IBAN numbers
+   /// </summary>
+   public interface IIBANConvert
+   {
+      /// <summary>
+      /// converts the parts of a national account number to an IBAN.
+      /// There are different parts needed in dependency of the selected country
+      /// </summary>
+      /// <param name="country">The country.</param>
+      /// <param name="parts">The parts.</param>
+      /// <returns></returns>
+      string ToIBAN(Country country, string[] parts);
+
+      /// <summary>
+      /// converts an IBAN to the parts of a national account number
+      /// </summary>
+      /// <param name="country">The country.</param>
+      /// <param name="iban">The iban.</param>
+      /// <returns></returns>
+      string[] FromIBAN(Country country, string iban);
+   }
+}
