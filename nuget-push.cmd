72d3bba269295a323e2904185ad3c6a4b51d8c37
micjahn <mikej06@hotmail.com>
2011-08-27 19:33:46 +0000

    version 0.4.0.0
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05]$/acconuts/trunk;C69461

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,7 +1,6 @@
 @ECHO OFF
 
 SET Id=AccountNumberTools
-SET VERSION=0.2.0.0
+SET VERSION=0.4.0.0
 
-3rdParty\nuget\nuget delete %ID% %VERSION%
 3rdParty\nuget\nuget push Build\nuget\%ID%.%VERSION%.nupkg
---
+++ nuget-remove.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+SET Id=AccountNumberTools
+SET VERSION=0.2.0.0
+
+3rdParty\nuget\nuget delete %ID% %VERSION%
